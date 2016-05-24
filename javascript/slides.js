6df651d5f38f05512d5831d869a865fe0578e0ed
Thomas-Rudge <thomas.rudge85@gmail.com>
2016-05-24 19:33:03 +0530

    Create photography page, update design page.
    Simplified the design page making it more responsive. Created the
    photography page as a clone of the design page, but still need to add
    the correct images.

--- javascript/slides.js
+++ javascript/slides.js
@@ -1,9 +1,61 @@
-// For the design.html page.
-// Makes the images appear in full with black background
 
+var currentId;   // Toggles a fullscreen translucent black div
+var currentPage; // Allows adjustment in slideshow behaviour (Design/Photo)
+var currentBody;
 
-// Toggles a fullscreen translucent black div
-var currentId ;
+// Respond to keyboard input
+document.onkeydown = function() {
+   if (event.defaultPrevented){
+      return;
+   }
+   
+   var key = event.which || event.key || event.keyCode;
+   var blackDiv = document.getElementById("blackdiv");
+   
+   switch(key) {
+      case 27: // Escape
+      case 'Escape':
+         contractImageView(currentId)
+         hideBlackDiv()
+         break;
+      case 36: // Home key
+      case 'Home':
+         scroll(0,0);
+         var banner = document.getElementById('DESIGN');
+         banner.style.opacity = 0;
+         banner.style.width = 'auto';
+         banner.style.position = 'absolute';
+         break;
+         
+      case 37: // Left arrow key
+      case 100:// Left arrow on numpad
+      case 'Left':
+      case '4':
+         if (blackDiv.style.display === 'none') {
+            return;
+         }
+         
+         imageToggle(-1);
+         break;
+         
+      case 39: // Right arrow key
+      case 102:// Right arrow on numpad
+      case 'Right':
+      case '6':
+         if (blackDiv.style.display === 'none') {
+            return;
+         }
+         
+         imageToggle(1);
+         break;
+         
+      default:
+         return;
+   }
+   
+   event.preventDefault();
+};
+
 
 var toggleBlackDiv = function() {
    var ele = document.getElementById("blackdiv");
@@ -19,6 +71,41 @@ var toggleBlackDiv = function() {
    }
 };
 
+// Because why not
+var rotatePhotoBanner = function() {
+   var ele = document.getElementById('PHOTO');
+   
+   if (!ele) {
+      return;
+   }
+   
+   var x = Math.random();
+   var imgSrc = ele.src;
+   
+   if (x >= 0.5) {
+      imgSrc = imgSrc.slice(0,-9) + 'main2.png';
+   } else {
+      imgSrc = imgSrc.slice(0,-9) + 'main1.png';
+   }
+   
+   ele.src = imgSrc;   
+};
+
+
+// A function to pad out a string with a given character in either right or left direction
+// padText = A string, padChar = A single character as a string, padLength = integer, padDirection = 'r' || 'l'
+var paddify = function(padText, padChar, padLength, padDirection) {
+   if (padText.length >= padLength) {
+      return padText.slice(0, padLength)
+   }
+   
+   if (padDirection === 'l') {
+      return padText + padChar.repeat(padLength - padText.length);
+   } else {
+      return padChar.repeat(padLength - padText.length) + padText;
+   }   
+};
+
 // Used by onresize because otherwise the toggleBlackDiv method would toggle
 // after each resize, regardless as to whether an image was present on screen.
 var hideBlackDiv = function() {
@@ -28,65 +115,95 @@ var hideBlackDiv = function() {
    ele.style.opacity = 0;
 };
 
-// Changes the the id of the 
+// Move the slideshow in a direction
+var imageToggle = function(direction) {
+   if (currentPage === 'DESIGN') {
+      var validIds = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14];
+      var divName = 'dimg';
+   } else {
+      var validIds = []
+      var divName = 'pimg';
+   }
+   
+   var currentImageInt = parseInt(currentId.replace('dimg','').replace('pimg',''));
+   var nextImageInt = currentImageInt + direction
+   
+   if (nextImageInt === 0) {
+      nextImageInt = validIds.slice(-1)[0];
+   } else if (nextImageInt > validIds.length) {
+      nextImageInt = 1;
+   }
+   
+   nextImageInt = divName + paddify(nextImageInt.toString(), '0', 2, 'r');
+   
+   contractImageView(currentId);
+   expandImageView(nextImageInt);
+};
+
+
 var expandImageView = function(imageId) {
    // Get the width of the page
-   var pageBody = document.getElementById('bdesign');
+   var pageBody = document.getElementById(currentBody);
    var pageBodyHeight = window.innerHeight;
    var pageBodyWidth = pageBody.offsetWidth;
    // Only do this if the screen is big enough
    if (pageBodyHeight > 550 && pageBodyWidth > 778) {
       // Get the image object
-      var image = document.getElementById(imageId);
+      var imageDiv = document.getElementById(imageId);
       // Change the images class.
-      image.className = 'active_design_div';
+      imageDiv.className = 'active_design_div';
       // Calculate the margins.
-      var imageWidth = image.offsetWidth;
-      var imageHeight = image.offsetHeight;
+      var imageWidth = imageDiv.offsetWidth;
+      var imageHeight = imageDiv.offsetHeight;
       var leftOffset = (pageBodyWidth - imageWidth) / 2;
       var topOffset = (pageBodyHeight - imageHeight) /2;
       // Adjust styling to make the div visible and centred.
-      image.style.left = String(leftOffset) + 'px';
-      image.style.top = String(topOffset) + 'px';
-      image.style.visibility = 'visible';
-      image.style.opacity = 1;
+      imageDiv.style.left = String(leftOffset) + 'px';
+      imageDiv.style.top = String(topOffset) + 'px';
+      imageDiv.style.visibility = 'visible';
+      imageDiv.style.opacity = 1;
       // Set the objects new onclick method to collapse the image
-      image.children[0].onclick = function() {contractImageView(imageId);};
+      imageDiv.children[0].onclick = function() {contractImageView(imageId);};
       //
       currentId = imageId;
       toggleBlackDiv();
    }
 };
 
-var contractImageView = function(imageId) {
-   var divs = [document.getElementById('design_div1'), document.getElementById('design_div2'), document.getElementById('design_div3'), document.getElementById('design_div4')];
-   var i;
-   var x;
-   
-   for (i = 0; i < divs.length; i++) {
-      for (x = 0; x < divs[i].children.length; x++) {
-         if (divs[i].children[x].className === 'active_design_div') {
-            divs[i].children[x].className = 'design_div';
-            divs[i].children[x].children[0].onclick = function() {expandImageView(imageId);};
-         }
-      }
-   }
 
+var contractImageView = function(imageId) {
+   active_id = document.getElementById(imageId);
+   
+   if (!active_id) {
+      return;
+   }else if (currentPage === 'DESIGN') {
+      var standardClass = 'design_div';
+   } else {
+      var standardClass = 'photo_div';
+   }
+   
+   active_id.className = standardClass;
+   active_id.children[0].onclick = function() {expandImageView(imageId)};
+   
    toggleBlackDiv();
 };
 
+
 var toggleBanner = function() {
-   var banner = document.getElementById('DESIGN');
-   var rect = banner.getBoundingClientRect()   
+   var banner = document.getElementById(currentPage);
+   var rect = banner.getBoundingClientRect();
+   // If the screen is too small
+   if (banner.style.textAlign === 'left') {
+      return;
    // If scroll up past the banner
-   if (rect['top'] > 10) {
+   } else if (document.body.scrollTop <= 130) {
       banner.style.opacity = 0;
       setTimeout(function(){
          banner.style.width = 'auto';
          banner.style.position = 'absolute';
       }, 100);
    // If scroll down past the header navbar
-   } else if (document.body.scrollTop > 110 || document.documentElement.scrollTop > 110) {
+   } else if (document.body.scrollTop > 140) {
       banner.style.position = 'inherit';
       banner.style.width = '100%';
       setTimeout(function(){
@@ -95,9 +212,19 @@ var toggleBanner = function() {
    }
 };
 
-var hideNoscript = function() {
+
+var onLoadActivities = function() {
    var ele = document.getElementById('noscript');
    ele.style.display = 'none';
+   
+   if (document.getElementById('DESIGN')) {
+      currentPage = 'DESIGN';
+      currentBody = 'bdesign';
+   } else {
+      currentPage = 'PHOTO';
+      currentBody = 'bphoto';
+      rotatePhotoBanner();
+   }
 };
 
 // This will collapse any open image on a resize.
@@ -105,4 +232,4 @@ window.onresize = function() {contractImageView(currentId); hideBlackDiv()};
 // This hides the banner if the person scrolls up to the main header
 window.onscroll = function() {toggleBanner()};
 // Remove the noscript div
-window.onload = function() {hideNoscript()};
+window.onload = function() {onLoadActivities()};
